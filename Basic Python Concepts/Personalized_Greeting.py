d52301e036660d4465060cff883307a099b54212
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-04-13 17:54:01 +0530

    Add personalized greeting script

---
+++ Basic Python Concepts/Personalized_Greeting.py
@@ -0,0 +1,11 @@
+# Prompt the user for their first name
+first_name = input("Enter your first name: ")
+
+# Prompt the user for their last name
+last_name = input("Enter your last name: ")
+
+# Concatenate first name and last name to form the full name
+full_name = first_name + " " + last_name
+
+# Print a personalized greeting message
+print(f"Hello, {full_name}! Welcome to python Program!")
