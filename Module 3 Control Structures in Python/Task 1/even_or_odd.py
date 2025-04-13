1b6e32a5b54a0471a798fd15676c8ef1127fb216
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-04-13 18:06:46 +0530

    Add program to check if a number is even or odd

---
+++ Module 3 Control Structures in Python/Task 1/even_or_odd.py
@@ -0,0 +1,10 @@
+# Program to check if a number is even or odd
+
+# Take integer input from the user
+number = int(input("Enter an integer: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")
