711aefed1327d8f628524bb8313cbb0d2b8169be
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-04-18 17:20:42 +0530

    Module 6 Demonstrating List slicing

---
+++ Module 6 Data Structures and Strings in Python/Task 2/Demonstrate_List_Slicing.py
@@ -0,0 +1,10 @@
+#Creates a list of numbers from 1 to 10.
+
+original_list = [1,2,3,4,5,6,7,8,9,10]
+
+# Extracts the first five elements from the list.
+Extracted_Elemnts = original_list[0:5]
+print(f'Extracting first five elements {Extracted_Elemnts}')
+
+# Reverses these extracted elements.
+print(f'Reversing extracted elements {Extracted_Elemnts[::-1]}')
